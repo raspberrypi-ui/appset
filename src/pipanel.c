0e83abc17bcdbbfa0de828e7d801672022ef18c6
Simon Long <simon@raspberrypi.com>
2021-01-21 15:16:37 +0000

    Remove spurious spaces

--- src/pipanel.c
+++ src/pipanel.c
@@ -2505,9 +2505,9 @@ int main (int argc, char *argv[])
 
 #ifdef ENABLE_NLS
     setlocale (LC_ALL, "");
-    bindtextdomain ( GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR );
-    bind_textdomain_codeset ( GETTEXT_PACKAGE, "UTF-8" );
-    textdomain ( GETTEXT_PACKAGE );
+    bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+    textdomain (GETTEXT_PACKAGE);
 #endif
 
     // check to see if lxsession will auto-refresh - version 0.4.9 or later
