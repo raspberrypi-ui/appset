f116d3b3eb98c3d7de9b39a6c14442f3f8d60548
Simon Long <simon@raspberrypi.com>
2023-07-14 12:36:47 +0100

    Restore original wayfire test

--- src/pipanel.c
+++ src/pipanel.c
@@ -2804,7 +2804,7 @@ int main (int argc, char *argv[])
     gtk_init (&argc, &argv);
     gtk_icon_theme_prepend_search_path (gtk_icon_theme_get_default(), PACKAGE_DATA_DIR);
 
-    if (getenv ("WAYLAND_DISPLAY")) wayfire = TRUE;
+    if (getenv ("WAYFIRE_CONFIG_FILE")) wayfire = TRUE;
 
     message (_("Loading configuration - please wait..."));
     if (wayfire) g_signal_connect (msg_dlg, "event", G_CALLBACK (event), NULL);
