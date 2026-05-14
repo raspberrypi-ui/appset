4b282f32f607fc5992f369f4d78f8c1f069a04bd
Simon Long <simon@raspberrypi.com>
2026-05-14 12:38:56 +0100

    Black makes more sense as a default text colour...

--- src/defaults.c
+++ src/defaults.c
@@ -842,6 +842,8 @@ static void enable_dock (void)
     g_key_file_set_string (kf, "panel", "layer", "bottom");
     g_key_file_set_boolean (kf, "panel", "exclusive", FALSE);
     g_key_file_set_string (kf, "dock", "widgets_left", "nmenu spacing0 tlist");
+    g_key_file_set_string (kf, "panel", "nmenu_overlay_text_col", "rgb(255,255,255)");
+
     str = g_key_file_to_data (kf, &len, NULL);
     g_file_set_contents (user_config_file, str, len, NULL);
     g_free (str);
